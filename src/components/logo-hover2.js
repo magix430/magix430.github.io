525e16273930c11481a0ed51db5f82562c447f6c
magix430 <magix430@gmail.com>
2019-06-01 06:57:02 -0500

    Update

---
+++ src/components/logo-hover2.js
@@ -0,0 +1,58 @@
+var firstFrame = "/src/images/freeze.gif";
+var lastFrame = "/src/images/final-frame.gif";
+var forwardAnimation = "/src/images/PersonalLogoAnimation.gif";
+var backwardAnimation = "/src/images/reversed.gif";
+
+var animationLength = 1200;
+var playing = false;
+var checkingIfPlaying = false;
+
+function playForward() {
+    if ($("#animation").is(":hover")) {
+        playing = true;
+        $("#animation").attr("src", forwardAnimation);
+        window.setTimeout(function () {
+            $("#animation").attr("src", lastFrame);
+            playing = false;
+        }, animationLength);
+    } else {
+        $("#animation").attr("src", firstFrame);
+    }
+}
+
+function playBackward() {
+    if ($("#animation").is(":hover")) {
+        $("#animation").attr("src", lastFrame);
+    } else {
+        playing = true;
+        $("#animation").attr("src", backwardAnimation);
+        window.setTimeout(function () {
+            $("#animation").attr("src", firstFrame);
+            playing = false;
+        }, animationLength);
+    }
+}
+
+$("#animation").mouseover(function (e) {
+    if (!checkingIfPlaying) {
+        checkingIfPlaying = window.setInterval(function () {
+            if (!playing) {
+                window.clearInterval(checkingIfPlaying);
+                playForward()
+                checkingIfPlaying = false;
+            }
+        }, 0);
+    }
+});
+
+$("#animation").mouseout(function (e) {
+    if (!checkingIfPlaying) {
+        checkingIfPlaying = window.setInterval(function () {
+            if (!playing) {
+                window.clearInterval(checkingIfPlaying);
+                playBackward()
+                checkingIfPlaying = false;
+            }
+        }, 0);
+    }
+});
